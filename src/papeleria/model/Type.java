66e6f865be9d14665ff4028a14d51405f2d77b99
Heber Haziel Nava Martinez <l21380416@cdvictoria.tecnm.mx>
2023-04-06 17:15:04 -0600

    TypeDTO y TablasBase

---
+++ src/papeleria/model/Type.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package papeleria.model;
+
+/**
+ *
+ * @author heber
+ */
+public class Type {
+
+    private int idType;
+    private String nameType;
+    private boolean exists;
+
+    public Type(TypeBuilder build) {
+        this.idType = build.idType;
+        this.nameType = build.nameType;
+        this.exists = build.exists;
+    }
+
+    public int getIdType() {
+        return idType;
+    }
+
+    public void setIdType(int idType) {
+        this.idType = idType;
+    }
+
+    public String getNameType() {
+        return nameType;
+    }
+
+    public void setNameType(String nameType) {
+        this.nameType = nameType;
+    }
+
+    public boolean isExists() {
+        return exists;
+    }
+
+    public void setExists(boolean exists) {
+        this.exists = exists;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder string = new StringBuilder();
+        string.append("Type{ ").append("idType= ").append(idType).append(", nameType= ").append(nameType).append(", exists= ").append(exists).append("}");
+        return string.toString();
+    }
+
+    public static class TypeBuilder {
+
+        private int idType;
+        private String nameType;
+        private boolean exists;
+
+        public TypeBuilder idType(int idType) {
+            this.idType = idType;
+            return this;
+        }
+
+        public TypeBuilder nameType(String nameType) {
+            this.nameType = nameType;
+            return this;
+        }
+
+        public TypeBuilder exists(boolean exists) {
+            this.exists = exists;
+            return this;
+        }
+
+        public Type build() {
+            return new Type(this);
+        }
+
+    }
+}
